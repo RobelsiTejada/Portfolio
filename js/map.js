524e12e84f0bb3ff3e9d93a7cf5f8044c48fb99f
RobelsiTejada <robelsitejada@gmail.com>
2017-08-21 13:54:12 -0400

    Adding fix to maps

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
 const map_wrapper = 'map_container'
 const longitude = 42.008299
 const latitude = -71.397398
-const bubble_content =	"<p class='map_contacts'>" + "<span>Address : </span>" +
-"Rhode Island/Massachusetts Area" + "</p>"
+const bubble_content = '<p class=map_contacts>' + '<span>Address : </span>' +
+'Rhode Island/Massachusetts Area' + '</p>'
 
 function initialize () {
   const mapOptions = {
@@ -11,27 +11,27 @@ function initialize () {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     mapTypeControl: false,
     streetViewControl: false,
-    scrollwheel : false,
+    scrollwheel: false,
     zoomControlOptions: {
       style: google.maps.ZoomControlStyle.SMALL
     }
   }
 
-	var map = new google.maps.Map(document.getElementById(map_wrapper),mapOptions);
+  const map = new google.maps.Map(document.getElementById(map_wrapper), mapOptions)
 
-	var marker = new google.maps.Marker({
-	  position:  new google.maps.LatLng(longitude, latitude),
-	  map: map,
-	  icon     : "img/marker.png"
-	});
+  const marker = new google.maps.Marker({
+    position: new google.maps.LatLng(longitude, latitude),
+    map: map,
+    icon: 'img/marker.png'
+  })
 
-	var infowindow = new google.maps.InfoWindow({
-			content: bubble_content
-		});
+  const infowindow = new google.maps.InfoWindow({
+    content: bubble_content
+  })
 
-	google.maps.event.addListener(marker, 'click', function() {
-	  infowindow.open(map,marker);
-	});
+  google.maps.event.addListener(marker, 'click', function () {
+    infowindow.open(map, marker)
+  })
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize)
