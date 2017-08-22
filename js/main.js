91b3d0c90658c021855fe7153867359f141a523f
RobelsiTejada <robelsitejada@gmail.com>
2017-08-22 01:07:49 -0400

    Adding new quote background photo

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ $(document).ready(function () {
   /*  Foundation Init    */
   $(document).foundation()
   /*  carousel Init    */
-  $("#carousel").carouFredSel({
-    width: "670",
-    pagination: ".pagination",
+  $('#carousel').carouFredSel({
+    width: '670',
+    pagination: '.pagination',
     responsive: true,
     scroll: {
-      fx: "fade"
+      fx: 'fade'
     },
     items: {
       visible: 1,
-      width: "670"
+      width: '670'
     },
     swipe: {
       onMouse: true,
@@ -19,15 +19,15 @@ $(document).ready(function () {
     }
   })
   /*    Mean navigation menu scroll to    */
-  $("#mean_nav ul li a").click(function (e) {
+  $('#mean_nav ul li a').click(function (e) {
     e.preventDefault()
-    scrollTo($(this).attr("href"), 900, "easeInOutCubic")
+    scrollTo($(this).attr('href'), 900, 'easeInOutCubic')
   })
   /*    Back to top button    */
-  const backTop = $("#back_top")
+  const backTop = $('#back_top')
   backTop.click(function (e) {
     e.preventDefault()
-    scrollTo(0, 900, "easeInOutCubic")
+    scrollTo(0, 900, 'easeInOutCubic')
   })
 
   function scrollTo (target, speed, ease) {
@@ -36,7 +36,7 @@ $(document).ready(function () {
     })
   }
 
-  $(window).on("scroll", function () {
+  $(window).on('scroll', function () {
     if ($(this).scrollTop() > 749) {
       backTop.stop().animate({
         opacity: 1
