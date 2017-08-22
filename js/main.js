f07ec7ab7179b83a5c134759f03a3ad877cefa5e
RobelsiTejada <robelsitejada@gmail.com>
2017-08-22 01:04:11 -0400

    Adding new quote background photo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
   /*  Foundation Init    */
-  $(document).foundation();
+  $(document).foundation()
   /*  carousel Init    */
   $('#carousel').carouFredSel({
     width: '670',
@@ -17,34 +17,34 @@ $(document).ready(function() {
       onMouse: true,
       onTouch: true
     }
-  });
+  })
   /*    Mean navigation menu scroll to    */
-  $('#mean_nav ul li a').click(function(e) {
-    e.preventDefault();
-    scrollTo($(this).attr('href'), 900, 'easeInOutCubic');
-  });
+  $('#mean_nav ul li a').click(function (e) {
+    e.preventDefault()
+    scrollTo($(this).attr('href'), 900, 'easeInOutCubic')
+  })
   /*    Back to top button    */
-  var back_top = $('#back_top');
-  back_top.click(function(e) {
-    e.preventDefault();
-    scrollTo(0, 900, 'easeInOutCubic');
-  });
+  const backTop = $('#back_top')
+  backTop.click(function (e) {
+    e.preventDefault()
+    scrollTo(0, 900, 'easeInOutCubic')
+  })
 
-  function scrollTo(target, speed, ease) {
+  function scrollTo (target, speed, ease) {
     $(window).scrollTo(target, speed, {
       easing: ease
-    });
+    })
   }
 
-  $(window).on('scroll', function() {
+  $(window).on('scroll', function () {
     if ($(this).scrollTop() > 749) {
-      back_top.stop().animate({
+      backTop.stop().animate({
         opacity: 1
-      }, 250);
+      }, 250)
     } else {
-      back_top.stop().animate({
+      backTop.stop().animate({
         opacity: 0
-      }, 250);
+      }, 250)
     }
-  });
-});
+  })
+})
